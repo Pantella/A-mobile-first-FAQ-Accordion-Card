6fc73e5d1bba8ac1966b051bd440437033890ffd
Pantella <88403089+Pantella@users.noreply.github.com>
2021-08-21 22:28:06 +0200

    Add files via upload
    Uploaded the not-minified version of CSS and JS files for better readability

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
-let lista_domande=document.querySelector("#faqAccordion_lst"),lista_risposte=document.getElementsByClassName("faqAccordion_answ");function nascondiAnsw(e){for(let t=0;t<e.length;++t)"exposed"==e[t].parentNode.dataset.state&&(e[t].parentNode.dataset.state="",e[t].classList.toggle("show"),e[t].previousElementSibling.lastElementChild.classList.remove("icon_rotate"))}function esponiAnsw(e,t){if("accordion_question"==t){e.nextElementSibling.classList.toggle("show"),e.parentNode.setAttribute("data-state","exposed");let t=e.lastElementChild;console.log(t),t.classList.add("icon_rotate")}}window.addEventListener("click",(e=>{console.log("Funzione innescata");let t=e.target.parentNode,s=t.dataset.action;nascondiAnsw(lista_risposte),esponiAnsw(t,s)}));
+let lista_domande=document.querySelector("#faqAccordion_lst");
+let lista_risposte=document.getElementsByClassName("faqAccordion_answ");
+
+window.addEventListener('click',(e)=>{
+    console.log("Funzione innescata");
+    let item=e.target;
+    let parent=item.parentNode;
+    let parent_attr=parent.dataset.action;
+
+    nascondiAnsw (lista_risposte);
+
+    esponiAnsw (parent,parent_attr);
+});
+
+function nascondiAnsw (risposte) {
+    //console.log("nascondiAnsw innescata");
+    for (let i=0;i<risposte.length;++i) {
+        //risposte[i].style.display="none";
+        if (risposte[i].parentNode.dataset.state=="exposed") {
+            risposte[i].parentNode.dataset.state="";
+            risposte[i].classList.toggle("show");
+            risposte[i].previousElementSibling.lastElementChild.classList.remove("icon_rotate");
+        }
+    }
+}
+
+function esponiAnsw (padre,padre_attr) {
+    if(padre_attr=="accordion_question") {
+        let risposta=padre.nextElementSibling;
+        //console.log(risposta);
+        risposta.classList.toggle("show");
+
+        let list_item=padre.parentNode;
+        list_item.setAttribute("data-state","exposed");
+
+        let icona=padre.lastElementChild;
+        console.log(icona);
+        icona.classList.add("icon_rotate");
+    }
+    
+}
